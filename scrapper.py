c74fe5122645c09996bb2cd453fee44e3e0707be
Miseq <krzychnowakowski@gmail.com>
2020-03-27 00:23:55 +0100

    dziala pobieranie i zapis, projekt nabiera ksztaltow

--- scrapper.py
+++ scrapper.py
@@ -4,6 +4,8 @@ import requests
 import os
 import datetime
 import tldextract
+import clint
+import validators
 
 class Scraper:
     def __init__(self):
@@ -49,9 +51,18 @@ class Scraper:
             self.data = bs(r.text)
         else:
             return "cosik nie dzialaa"
-        imgs = self.data.find_all("img")
-        imgs_urls = [ link['src'] for link in imgs]
-        return 0
+        imgs = self.data.find_all("img",{"src":True})
+        imgs_urls = [ link['src'] for link in imgs if link['src'] != ""]
+        valid_urls = self.check_image_urls(imgs_urls)
+
+        #TODO DO ZIPA i dodac liste urls pobranych
+
+        for i, image in enumerate(valid_urls):
+            file_extension = image.split(".")[-1]
+            if len(file_extension)>4:
+                file_extension = "png"
+            with open(f"downloads/image_{i}.{file_extension}", "wb") as png:
+                png.write(requests.get(image, stream=True).content)
 
     def make_main_dwnld_dir(self):
         if not os.path.exists('soup_downloads'):
@@ -66,3 +77,43 @@ class Scraper:
         dir_name = f"{domain_part}--{date_time_part}"
         if not os.path.exists('soup_downloads/dir_name'):
             os.mkdir('soup_downloads/dir_name')
+
+    def check_image_urls(self, images):
+        valid_urls = []
+        for i, image_url in enumerate(images):
+            if not validators.url(image_url):
+                image_url = self.repair_invalid_image_url(image_url)
+                if image_url == "0":
+                    continue
+            valid_urls.append(image_url)
+        return valid_urls
+
+
+    def repair_invalid_image_url(self, url):
+        # cztery najczestsze napotkane podczas robienia zadania bledy w url
+        tmp_url = url
+        if url[0:2] == "//":
+            tmp_url = f"http:{url}"
+        elif url[0] == "/":
+            tmp_url = f"http:/{url}"
+        elif url[0:8] != "https://" and url[0:7] != "http://":
+            tmp_url = f"http://{url}"
+
+        # wystepujace rzadziej: brak domeny przed adresem i brak przekierowania z http:
+        if not validators.url(tmp_url):
+            ext = tldextract.extract(self.url)
+            ext_list = [part for part in ext if part != ""]
+            domain = '.'.join(ext_list[:])
+            tmp_url = f"http://{domain}{url}"
+
+
+        if not validators.url(tmp_url): # jesli usterka nie zostala usunieta
+            manual_validate = input(f"URL: {tmp_url} seems to be invalid, do you want to manually repair it? Y/N")
+            if manual_validate.lower() == "y":
+                while not validators.url(tmp_url) or url == "0":
+                    tmp_url = input("Enter correct url or type 0 to end and remove it from list").replace(" ","")
+            else:
+                tmp_url = "0"
+
+        return tmp_url # should be valid OR 0
+
